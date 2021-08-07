70fe6b410bc7c2ba392cb9aa0334598e99eb03cb
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-07 13:13:04 +0900

    sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use image::{Pixel, Rgba};
+use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::env;
 
@@ -6,9 +8,27 @@ fn main() {
     let image = image::open(&path)
         .expect("Failed to open the image.")
         .to_rgba8();
-    let pixels = image.pixels().collect::<HashSet<_>>();
+    let mut pixels = image
+        .pixels()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect::<Vec<_>>();
+    pixels.sort_by(|a, b| cmp_rgba(a, b));
 
     for pixel in pixels {
         println!("({}, {}, {}, {})", pixel[0], pixel[1], pixel[2], pixel[3]);
     }
+}
+
+fn cmp_rgba(a: &Rgba<u8>, b: &Rgba<u8>) -> Ordering {
+    let a_sum = a.channels().iter().map(|v| u16::from(*v)).sum::<u16>();
+    let b_sum = b.channels().iter().map(|v| u16::from(*v)).sum::<u16>();
+    if a_sum == b_sum {
+        return Ordering::Equal;
+    }
+    return if a_sum > b_sum {
+        Ordering::Greater
+    } else {
+        Ordering::Less
+    };
 }
