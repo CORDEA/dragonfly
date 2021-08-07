49a363304f20d9a5c7d94e9d9faac9c11b6138b4
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-07 12:26:04 +0900

    get image colors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::collections::HashSet;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let path = env::args().nth(1).expect("Target image is required.");
+    let image = image::open(&path)
+        .expect("Failed to open the image.")
+        .to_rgba8();
+    let pixels = image.pixels().collect::<HashSet<_>>();
+
+    for pixel in pixels {
+        println!("({}, {}, {}, {})", pixel[0], pixel[1], pixel[2], pixel[3]);
+    }
 }
