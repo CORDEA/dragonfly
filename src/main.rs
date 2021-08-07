e20b13c2db401c66a9ca414ee05a2082c852242e
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-07 13:45:44 +0900

    print in hex

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Clap;
-use image::{Pixel, Rgba};
+use image::Pixel;
 use std::cmp::Ordering;
 use std::collections::HashSet;
 
@@ -20,25 +20,37 @@ fn main() {
         .pixels()
         .collect::<HashSet<_>>()
         .into_iter()
+        .map(|v| v.channels())
         .collect::<Vec<_>>();
     pixels.sort_by(|a, b| cmp_rgba(a, b));
 
-    for pixel in pixels {
-        println!(
-            "({})",
-            pixel
-                .channels()
-                .iter()
+    if opts.hex {
+        pixels
+            .iter()
+            .map(|p| {
+                p.iter()
+                    .map(|v| format!("{:02X}", v))
+                    .collect::<Vec<_>>()
+                    .join("")
+            })
+            .for_each(|p| println!("#{}", p));
+        return;
+    }
+
+    pixels
+        .iter()
+        .map(|p| {
+            p.iter()
                 .map(|v| v.to_string())
                 .collect::<Vec<_>>()
                 .join(", ")
-        );
-    }
+        })
+        .for_each(|p| println!("({})", p));
 }
 
-fn cmp_rgba(a: &Rgba<u8>, b: &Rgba<u8>) -> Ordering {
-    let a_sum = a.channels().iter().map(|v| u16::from(*v)).sum::<u16>();
-    let b_sum = b.channels().iter().map(|v| u16::from(*v)).sum::<u16>();
+fn cmp_rgba(a: &[u8], b: &[u8]) -> Ordering {
+    let a_sum = a.iter().map(|v| u16::from(*v)).sum::<u16>();
+    let b_sum = b.iter().map(|v| u16::from(*v)).sum::<u16>();
     if a_sum == b_sum {
         return Ordering::Equal;
     }
